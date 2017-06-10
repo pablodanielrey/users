36a498fef8c00bbd1c6f4a50fbc9288fd3496532
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-06-10 12:37:06 -0300

    agregue ahora un ejemplo con usuario

--- docker/src/users/model/entities/User.py
+++ docker/src/users/model/entities/User.py
@@ -1,44 +1,62 @@
-import uuid
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+import datetime
+from sqlalchemy import Column, Integer, String, Date, DateTime, func
 
-from flask_jsontools import JsonSerializableBase
-
-Base = declarative_base(cls=(JsonSerializableBase,))
-
+from users.model.entities import generate_id, Base
 
 class User(Base):
 
     __tablename__ = 'users'
 
-    id = Column(String, primary_key=True, default=generate_uuid)
-    number = Column(String)
-    type = Column(String)
+    id = Column(String, primary_key=True, default=generate_id)
+    dni = Column(String)
+    name = Column(String)
+    lastname = Column(String)
+    gender = Column(String)
+    birthdate = Column(Date)
+    city = Column(String)
+    country = Column(String)
+    address = Column(String)
+    residence_city = Column(String)
+    created = Column(DateTime, server_default=func.now())
+    updated = Column(DateTime, onupdate=func.now())
 
-    @classmethod
-    def findAll(cls, s):
-        return s.query(cls).all()
 
 
-    def __init__(self):
-        self.id = None
-        self.dni = None
-        self.name = None
-        self.lastname = None
-        self.gender = None
-        self.birthdate = None
-        self.city = None
-        self.country = None
-        self.address = None
-        self.residence_city = None
-        self.created = datetime.datetime.now()
-        self.version = 0
-        self.photo = None
-        self.type = None
-        self.telephones = []
-        self.type = None
+if __name__ == '__main__':
+    import logging
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
+    engine = create_engine('postgresql://postgres:clavesecreta@localhost:5432/testing')
+    Base.metadata.create_all(engine)
+
+    Sm = sessionmaker(bind=engine)
+    s = Sm()
+    s.add_all([
+        User(dni='27294557', name='Pablo Daniel', lastname='Rey'),
+        User(dni='27294558', name='Pablo Daniel1', lastname='Rey'),
+        User(dni='27294559', name='Pablo Daniel2', lastname='Rey'),
+        User(dni='27294550', name='Pablo Daniel3', lastname='Rey')
+    ])
+    s.commit()
+    for t in s.query(User).all():
+        print(t.__json__())
+    #logging.info(Telephone.findAll(s))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    """
     def getAge(self):
         today = datetime.datetime.now()
         born = self.birthdate
@@ -59,11 +77,13 @@ class User(Base):
     @classmethod
     def findPhotos(cls, ctx, userIds):
         return ctx.dao(cls).findPhotos(ctx, userIds)
+    """
 
-
+"""
 class Student(User, Entity):
 
     def __init__(self):
         super().__init__()
         self.studentNumber = None
         self.condition = None
+"""
