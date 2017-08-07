c080c3e64dff562d932a2bf086ce43857a408c02
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-07 15:47:29 -0300

    segui con la pantalla de cambio de perfil

--- docker/src/users/web/componentes/perfil/controller.js
+++ docker/src/users/web/componentes/perfil/controller.js
@@ -1,15 +1,35 @@
 
 app.controller("PerfilCtrl", ["$scope", "$location", "$routeParams", "$resource", "$timeout", function ($scope, $location, $routeParams, $resource, $tiemout) {
 
+  $scope.parm = $routeParams;
+
   $scope.usuario = {
     nombre:'',
     apellido:'',
-    dni:$routeParams[dni],
+    dni:$routeParams['dni'],
     mails:[]
   }
   $scope.mensaje = 'Cargando';
+  $scope.emailAAgregar = '';
 
-  var Usuarios = $resource('http://192.168.0.3:6001/users/api/v1.0/usuarios?d=:dni', {dni:null});
-  $scope.usuario = Usuarios.get({dni:$routeParams[dni]});
+  var Usuarios = $resource('http://127.0.0.1:6001/users/api/v1.0/usuarios?d=:dni', {dni:null});
+  $scope.usuario = Usuarios.get({dni:$routeParams['dni']});
+
+
+  $scope.actualizarUsuario = function(usuario) {
+    alert(usuario.nombre);
+  };
+
+  $scope.eliminarCorreo = function(email) {
+    alert(email.email);
+  };
+
+  $scope.confirmarCorreo = function(email) {
+    alert(email.id);
+  }
+
+  $scope.agregarCorreo = function() {
+    alert($scope.emailAAgregar);
+  }
 
 }]);
