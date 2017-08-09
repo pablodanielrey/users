188cdcd43a32c96b20fd64f57d0898137bc56b8f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-09 11:49:20 -0300

    agregue para tner para crear las tablas cuando sea la hora de pasarlo a producción

--- docker/src/users/model/__init__.py
+++ docker/src/users/model/__init__.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.schema import CreateSchema
 from sqlalchemy.orm import sessionmaker
 
+from .entities import *
+
 from model_utils import Base
 
 engine = create_engine('postgresql://{}:{}@{}:5432/{}'.format(
@@ -19,3 +21,12 @@ from .UsersModel import UsersModel
 __all__ = [
     'UsersModel'
 ]
+
+"""
+por ahora usa las tablas generadas en los otros sistemas
+def crear_tablas():
+    from sqlalchemy.schema import CreateSchema
+
+    engine.execute(CreateSchema(''))
+    Base.metadata.create_all(engine)
+"""
