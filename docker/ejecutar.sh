438506a138faf2d5dca12ea2128eac5d17403e5f
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-08-22 20:13:15 -0300

    agregue el script de producción y el archivo en el gitlab

--- docker/ejecutar.sh
+++ docker/ejecutar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -ti --name $1 -p 5000:5000 --rm --env-file environment $1 $2
+sudo docker run -ti -d --name users -v $(pwd)/src:/src -p 5005:5000 -p 5006:5001 -p 5007:5002 -p 5008:5003 --env-file /home/pablo/gitlab/fce/produccion/users users
