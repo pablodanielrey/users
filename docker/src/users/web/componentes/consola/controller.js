e8ebb64c01853cf8419aa44eea901fc091f6e78b
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-09-04 17:44:23 -0300

    haciendo pruebas con la consola

--- docker/src/users/web/componentes/consola/controller.js
+++ docker/src/users/web/componentes/consola/controller.js
@@ -1,110 +1,21 @@
-app.controller("ConsolaCtrl", ["$scope", "$location", "$routeParams", "$resource", "$timeout", function ($scope, $location, $routeParams, $resource, $tiemout) {
+app.controller("ConsolaCtrl", ["$scope", "$location", "$window", "$route", function ($scope, $location, $window, $route) {
 
   $scope.cambiarConsola = function() {
     $scope.mostrar = !$scope.mostrar;
   }
 
-  // -------------- manejo de pantallas y errores ------------------------------------------------------ //
-
-  $scope.$parent.estado = '';
-  $scope.$parent.error = {
-    error: '',
-    codigo: ''
-  }
-  $scope.$parent.mensaje = {
-    mensaje: '',
-    codigo: ''
-  };
-
-  $scope.estado_actual = 0;
-  $scope.mensaje_actual = 0;
+  $scope.actual = 0;
 
   $scope.estadoSiguiente = function() {
-    $scope.estado_actual = ($scope.estado_actual + 1) % $scope.$parent.estados.length;
-    $scope.$parent.estado = $scope.$parent.estados[$scope.estado_actual];
+    $scope.actual = ($scope.actual + 1) % $route.routes.length;
+    console.log($route.routes[$scope.actual]);
+    $location.path($route.routes[$scope.actual].originalPath);
   }
 
   $scope.estadoAnterior = function() {
-    $scope.estado_actual = ($scope.estado_actual + $scope.$parent.estados.length - 1) % $scope.$parent.estados.length;
-    $scope.$parent.estado = $scope.$parent.estados[$scope.estado_actual];
+
   }
 
-
-
-  $scope.$parent.mensajeSiguiente = function() {
-    $scope.mensaje_actual = ($scope.mensaje_actual + 1) % $scope.$parent.mensajes.length;
-    $scope.setearMensaje($scope.$parent.mensajes[$scope.mensaje_actual]);
-  }
-
-  $scope.$parent.mensajeAnterior = function() {
-    $scope.mensaje_actual = ($scope.mensaje_actual + $scope.$parent.mensajes.length - 1) % $scope.$parent.mensajes.length;
-    $scope.setearMensaje($scope.$parent.mensajes[$scope.mensaje_actual]);
-  }
-
-  $scope.$parent.limpiarMensaje = function() {
-    $scope.$parent.mensaje = {
-      mensaje: '',
-      codigo:''
-    }
-  }
-
-  $scope.$parent.setearMensaje = function(m) {
-    $scope.$parent.mensaje = {
-      mensaje: 'mensaje',
-      codigo: m
-    }
-  }
-
-  $scope.$parent.cambiarMensaje = function() {
-    if ($scope.$parent.mensaje.mensaje == '') {
-      $scope.$parent.mensaje.mensaje = 'mensaje';
-    } else {
-      $scope.$parent.mensaje.mensaje = '';
-    }
-  }
-
-
-
-
-  $scope.$parent.limpiarError = function() {
-    $scope.$parent.error = {
-      error: '',
-      codigo: ''
-    }
-  }
-
-  $scope.$parent.setearError = function(e) {
-    $scope.$parent.error = {
-      error: 'error',
-      codigo: e.error
-    };
-  }
-
-  $scope.error_actual = 0;
-
-  $scope.$parent.cambiarError = function() {
-    if ($scope.$parent.error.error == '') {
-      $scope.$parent.error.error = 'error';
-    } else {
-      $scope.$parent.error.error = '';
-    }
-  }
-
-  $scope.$parent.errorSiguiente = function() {
-    $scope.error_actual = ($scope.error_actual + 1) % $scope.errores_posibles.length;
-    $scope.$parent.error = {
-      error: 'error',
-      codigo: $scope.$parent.errores_posibles[$scope.error_actual]
-    };
-  }
-
-  $scope.$parent.errorAnterior = function() {
-    $scope.error_actual = ($scope.error_actual + $scope.errores_posibles.length - 1) % $scope.errores_posibles.length;
-    $scope.$parent.error = {
-      error: 'error',
-      codigo: $scope.$parent.errores_posibles[$scope.error_actual]
-    };
-  }
 
 }]);
   ////////
