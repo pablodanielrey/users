d2f4efc9416fe1f77b0f24d61907710033e5e022
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-09-27 23:10:50 -0300

    agregue scripts para registrar el servicio en el registri

---
+++ docker/src/users/registrar.py
@@ -0,0 +1,14 @@
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+
+    dominio = os.environ['USERS_URL'].replace('http://','').replace('https://','')
+    nombre = sys.argv[1]
+    path = sys.argv[2]
+    server = sys.argv[3]
+
+    import auth_utils
+    r = auth_utils.RegistrarServicio()
+    r.register(name=nombre, domain=dominio, path=path, server=server)
