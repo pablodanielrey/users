82335e0e7b2446bb828c316b73d10472d6920bdf
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-10-12 15:59:05 -0300

    me faltaba el contains_eager

--- docker/src/users/model/UsersModel.py
+++ docker/src/users/model/UsersModel.py
@@ -131,11 +131,16 @@ class UsersModel:
             Usuario.apellido.op('~*')(search)\
         )) if search else q
         q = q.filter(or_(Usuario.actualizado >= fecha, Usuario.creado >= fecha)) if fecha else q
+
         #q = q.options(joinedload('claves')) if retornarClave else q
-        q = q.join(UsuarioClave).filter(UsuarioClave.eliminada == None)
+        if retornarClave:
+            q = q.join(UsuarioClave).filter(UsuarioClave.eliminada == None).options(contains_eager(Usuario.claves))
+
         q = q.options(joinedload('telefonos'))
+
         q = q.join(Mail).filter(Mail.eliminado == None)
         q = q.options(contains_eager(Usuario.mails))
+
         q = cls._aplicar_filtros_comunes(q, offset, limit)
         return q.all()
 
