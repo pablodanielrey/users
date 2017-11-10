e9a559b1ae4981a7f2ec0b7d22849165a9a4f830
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-11-10 12:22:36 -0300

    me faltaba el cambiar la version

--- docker/src/setup.py
+++ docker/src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='users',
-          version='1.0.0.alpha0',
+          version='1.0.1.alpha0',
           description='Proyecto que maneja el equema de usuarios de los sistemas',
           url='https://github.com/pablodanielrey/users',
           author='Desarrollo DiTeSi, FCE',
