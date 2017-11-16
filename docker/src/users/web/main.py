a782d6ad495740f51af733442cb38f425c3bb124
Pablo Daniel Rey <pablodanielrey@gmail.com>
2017-11-16 14:01:31 -0300

    cambiamos el dominio y id de la cookie de session

--- docker/src/users/web/main.py
+++ docker/src/users/web/main.py
@@ -39,8 +39,8 @@ app.config['OIDC_COOKIE_SECURE'] = False
 app.config['OIDC_VALID_ISSUERS'] = [os.environ['LOGIN_OIDC_ISSUER']]
 app.config['OIDC_RESOURCE_CHECK_AUD'] = False
 app.config['OIDC_INTROSPECTION_AUTH_METHOD'] = 'client_secret_post'
-app.config['OIDC_ID_TOKEN_COOKIE_NAME'] = 'users_oidc'
-#app.config['OIDC_ID_TOKEN_COOKIE_DOMAIN'] = '.econo.unlp.edu.ar'
+app.config['OIDC_ID_TOKEN_COOKIE_NAME'] = 'oidc'
+app.config['OIDC_ID_TOKEN_COOKIE_DOMAIN'] = '.econo.unlp.edu.ar'
 app.config['OIDC_USER_INFO_ENABLED'] = True
 app.config['OIDC_SCOPES'] = ['openid','email','phone','profile','address','econo']
 
