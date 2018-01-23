3533cbb9545eba7d041462c5e11147e59e233966
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-01-23 14:30:35 -0300

    movi de lugar el src para que quede mas coherente

--- docker/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-tar -cvzf src.tar.gz ./src
-sudo docker build -t users .
--- docker/ejecutar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker run -ti -d --name users -v $(pwd)/src:/src -p 5005:5000 -p 5006:5001 -p 5007:5002 -p 5008:5003 --env-file $HOME/gitlab/fce/produccion/users users
-sudo docker exec -t users bash instalar.sh
-sudo docker restart users
